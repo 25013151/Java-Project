3ffb57039cf84f6d7109e509f258e505f077cb9f
mususu138 <mabalav6@gmail.com>
2025-06-08 03:22:59 +0200

    First commit

---
+++ src/main/java/com/mycompany/numbers/Numbers.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.numbers;
+
+/**
+ *
+ * @author mabal
+ */
+public class Numbers {
+
+    public static void main(String[] args) {
+        
+      for(int i =1;i<10;i++){
+          System.out.println("i");
+      } 
+          for(int i =2;i<10;i++){
+      
+              System.out.println("Even numbers");      
+      }
+              for(int i =9;i<1;i--){
+              System.out.println("Odd numbers");
+                  
+                 
+              }}}
+          
+    
+          
+    
+
